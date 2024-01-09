cd531f9f783e032e2b4e9e77cc64498e87509a9a
piotr kotulski <piotr.kotulski@exorigo-upos.pl>
2024-01-10 00:29:11 +0100

    added delete post founctionality with modal

--- src/components/pages/SInglePost/SinglePost.js
+++ src/components/pages/SInglePost/SinglePost.js
@@ -1,42 +1,76 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { getAllPosts } from '../../../redux/postRedux';
-import { Link } from 'react-router-dom';
-import {Button, Card, Container} from 'react-bootstrap';
-import styles from './SinglePost.module.scss'
+import React, { useState } from 'react';
+import { useParams, Link, Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllPosts, deletePostSuccess, deletePostFailure } from '../../../redux/postRedux'; // Zmieniono importy akcji
+import { Button, Container, Modal, Card } from "react-bootstrap";
+import styles from "./SinglePost.module.scss";
 
 const SinglePost = () => {
     const { id } = useParams();
     const posts = useSelector(getAllPosts);
-    const post = posts.find((post) => post.id === id);
+    const post = posts.find(post => post.id === id);
+    const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false);
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    const handleShowModal = () => {
+        setShowModal(true);
+    };
+
+    const handleDeleteClick = () => {
+        // Pokaż okno modalne z potwierdzeniem usuwania
+        handleShowModal();
+    };
+
+    const handleConfirmDelete = () => {
+        // Wywołaj akcję usuwania posta
+        dispatch(deletePostSuccess(id)); // Użyj deletePostSuccess zamiast deletePostAction
+    };
 
     if (!post) {
-        return <div>Post not found</div>;
+        return <Navigate to="/" />;
     }
 
     return (
-        <Container>
-            <div className='d-flex justify-content-between'>
-            <Card className={styles.singlePagePreview}>
-                <Card.Body >
-                    <Card.Title as='h3'>{post.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 mt-3 text-muted">Author: {post.author}</Card.Subtitle>
-                    <Card.Subtitle className="mb-2 text-muted">Published: {post.publishedDate}</Card.Subtitle>
-                    <Card.Text>{post.shortDescription}</Card.Text>
-                </Card.Body>
-            </Card>
-                <div className='d-flex'>
-            <Link to={`/post/edit/${id}`}>
-                <Button className='w-100 me-4' variant="primary">Edit</Button>
-            </Link>
-                    <div>
-            <Button className='w-100 ms-2' variant="danger">Delete</Button>
-                    </div>
+        <div className='d-flex'>
+            <Container>
+                <Card className={styles.singlePagePreview}>
+                    <Card.Body>
+                        <Card.Title as="h3">{post.title}</Card.Title>
+                        <Card.Subtitle className="mb-2 mt-4 text-muted">Author: {post.author}</Card.Subtitle>
+                        <Card.Subtitle className="mb-4 text-muted">Published: {post.publishedDate}</Card.Subtitle>
+                        <Card.Text>{post.shortDescription}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Container>
+            <div className='d-flex ms-2'>
+                <Link to={`/post/edit/${id}`}>
+                    <Button className='w-150' variant="primary">EDIT</Button>
+                </Link>
+                <div>
+                    <Button className='ms-2' variant="danger" onClick={handleDeleteClick}>DELETE</Button>
                 </div>
             </div>
-        </Container>
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm Deletion</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure you want to delete this post?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Cancel
+                    </Button>
+                    <Button variant="danger" onClick={handleConfirmDelete}>
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
 };
 
 export default SinglePost;
+
--- src/redux/postRedux.js
+++ src/redux/postRedux.js
@@ -1,16 +1,35 @@
 import initialState from './initialState';
 
-
-//selectors
-
+// Selektory
 export const getAllPosts = state => state.posts;
 
-// actions
-//const createActionName = actionName => `app/posts/${actionName}`;
+// Akcje
+const DELETE_POST_SUCCESS = 'app/posts/DELETE_POST_SUCCESS';
+const DELETE_POST_FAILURE = 'app/posts/DELETE_POST_FAILURE';
 
-// action creators
+// Akcje kreatorów
+export const deletePostSuccess = (postId) => ({
+    type: DELETE_POST_SUCCESS,
+    payload: postId,
+});
+
+export const deletePostFailure = (error) => ({
+    type: DELETE_POST_FAILURE,
+    payload: error,
+});
+
+// Reducer
 const postsReducer = (statePart = initialState, action) => {
     switch (action.type) {
+        case DELETE_POST_SUCCESS: {
+            const postId = action.payload;
+            const updatedPosts = statePart.filter(post => post.id !== postId);
+            return updatedPosts;
+        }
+        case DELETE_POST_FAILURE: {
+            // Obsłuż błąd usuwania posta (jeśli potrzebne)
+            return statePart;
+        }
         default:
             return statePart;
     }
